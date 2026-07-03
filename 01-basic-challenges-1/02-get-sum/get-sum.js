0e2c090b7ef8c2a6d11d398998017624e7e4a7dd
M Fahad Ali <mfahaad.ali9@gmail.com>
2026-07-03 11:38:11 +0500

    basic challange get-sum complete

--- 01-basic-challenges-1/02-get-sum/get-sum.js
+++ 01-basic-challenges-1/02-get-sum/get-sum.js
@@ -1,3 +1,13 @@
-function getSum() {}
+/**
+ *
+ * @param{number} a - The first number.
+ * @param{number} b - The second number.
+ * @returns{number} The result sum of two numbers.
+ *
+ */
+
+function getSum(a, b) {
+  return a + b;
+}
 
 module.exports = getSum;
