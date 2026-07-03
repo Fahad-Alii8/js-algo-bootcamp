6c0ad5e472ec75257dbb56d21b968f9c9f831f88
M Fahad Ali <mfahaad.ali9@gmail.com>
2026-07-03 17:17:06 +0500

    basic challange calculator completed

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,43 @@
-function calculator() {}
+/**
+ *
+ * @param{number} num1 - The first number.
+ * @param{number} num2 - The second number.
+ * @param{string} operator - The operator to use in the calculation.
+ * @returns{number} The result of the calculation.
+ *
+ */
+
+// function calculator(num1, num2, operator) {
+//   if (operator === "+") {
+//     return num1 + num2;
+//   } else if (operator === "-") {
+//     return num1 - num2;
+//   } else if (operator === "*") {
+//     return num1 * num2;
+//   } else if (operator === "/") {
+//     return num1 / num2;
+//   } else {
+//     throw new Error("Invalid Operator");
+//   }
+// }
+
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+      break;
+    case "-":
+      return num1 - num2;
+      break;
+    case "*":
+      return num1 * num2;
+      break;
+    case "/":
+      return num1 / num2;
+      break;
+    default:
+      throw new Error("Invalid Operator");
+  }
+}
 
 module.exports = calculator;
