133221f7900484f60b0bd13a61d1747c9e89c767
M Fahad Ali <mfahaad.ali9@gmail.com>
2026-07-31 22:45:59 +0500

    count occurrence with both methods

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,22 @@
-function countOccurrences() {}
+// function countOccurrences(str, char) {
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i].toLowerCase() === char) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+function countOccurrences(str, char) {
+  let count = 0;
+  const splittedString = str.split("");
+  for (let letter of splittedString) {
+    if (letter === char) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countOccurrences;
